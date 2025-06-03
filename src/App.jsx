1601e088f6715fc72f0cc0d98000a451d2ad0fa9
Bronwyn Taljaard <bronwyntaljaard@bronwyns-laptop.home>
2025-06-03 16:45:11 +0200

    Updated UserInput component - changed input groups from divs to paragraph elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Result from "./components/Result";
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Result from "./components/Result.jsx";
 
 import { calculateInvestmentResults } from "./util/investment";
 
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -6,56 +6,58 @@ export default function UserInput({
 	handleChange,
 }) {
 	return (
-		<form id="user-input">
-			<div className="input-group">
-				<div>
-					<label htmlFor="initial-investment">Initial investment</label>
-					<input
-						type="number"
-						id="initial-investment"
-						name="initialInvestment"
-						value={initialInvestment}
-						onChange={handleChange}
-						required
-					></input>
+		<section id="user-input">
+			<form id="user-input">
+				<div className="input-group">
+					<p>
+						<label htmlFor="initial-investment">Initial investment</label>
+						<input
+							type="number"
+							id="initial-investment"
+							name="initialInvestment"
+							value={initialInvestment}
+							onChange={handleChange}
+							required
+						></input>
+					</p>
+					<p>
+						<label htmlFor="annual-investment">Annual investment</label>
+						<input
+							type="number"
+							id="annual-investment"
+							name="annualInvestment"
+							value={annualInvestment}
+							onChange={handleChange}
+							required
+						></input>
+					</p>
 				</div>
-				<div>
-					<label htmlFor="annual-investment">Annual investment</label>
-					<input
-						type="number"
-						id="annual-investment"
-						name="annualInvestment"
-						value={annualInvestment}
-						onChange={handleChange}
-						required
-					></input>
+				<div className="input-group">
+					<p>
+						<label htmlFor="expected-return">Expected return</label>
+						<input
+							type="number"
+							id="expected-return"
+							name="expectedReturn"
+							step="0.1"
+							required
+							value={expectedReturn}
+							onChange={handleChange}
+						></input>
+					</p>
+					<p>
+						<label htmlFor="duration">Duration</label>
+						<input
+							type="number"
+							id="duration"
+							name="duration"
+							required
+							value={duration}
+							onChange={handleChange}
+						></input>
+					</p>
 				</div>
-			</div>
-			<div className="input-group">
-				<div>
-					<label htmlFor="expected-return">Expected return</label>
-					<input
-						type="number"
-						id="expected-return"
-						name="expectedReturn"
-						step="0.1"
-						required
-						value={expectedReturn}
-						onChange={handleChange}
-					></input>
-				</div>
-				<div>
-					<label htmlFor="duration">Duration</label>
-					<input
-						type="number"
-						id="duration"
-						name="duration"
-						required
-						value={duration}
-						onChange={handleChange}
-					></input>
-				</div>
-			</div>
-		</form>
+			</form>
+		</section>
 	);
 }
