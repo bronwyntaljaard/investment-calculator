b94154f066ac21dd90ffa38de737ad465e9d263c
Bronwyn Taljaard <bronwyntaljaard@bronwyns-laptop.home>
2025-06-10 14:50:35 +0200

    removed unnecessary fragment in Result.jsx

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,38 +2,36 @@ import { formatter } from "../util/investment";
 
 export default function Result({ result, initialInvestment }) {
 	return (
-		<>
-			<table id="result">
-				<thead>
-					<tr>
-						<th>Year</th>
-						<th>Investment Value</th>
-						<th>Interest (Year)</th>
-						<th>Total Interest</th>
-						<th>Invested Capital</th>
+		<table id="result">
+			<thead>
+				<tr>
+					<th>Year</th>
+					<th>Investment Value</th>
+					<th>Interest (Year)</th>
+					<th>Total Interest</th>
+					<th>Invested Capital</th>
+				</tr>
+			</thead>
+			<tbody>
+				{Object.entries(result).map(([index, entry]) => (
+					<tr key={index}>
+						<td>{entry.year}</td>
+						<td>{formatter.format(entry.valueEndOfYear)}</td>
+						<td>{formatter.format(entry.interest)}</td>
+						<td>
+							{formatter.format(
+								entry.valueEndOfYear -
+									(entry.year * entry.annualInvestment + initialInvestment)
+							)}
+						</td>
+						<td>
+							{formatter.format(
+								entry.year * entry.annualInvestment + initialInvestment
+							)}
+						</td>
 					</tr>
-				</thead>
-				<tbody>
-					{Object.entries(result).map(([index, entry]) => (
-						<tr key={index}>
-							<td>{entry.year}</td>
-							<td>{formatter.format(entry.valueEndOfYear)}</td>
-							<td>{formatter.format(entry.interest)}</td>
-							<td>
-								{formatter.format(
-									entry.valueEndOfYear -
-										(entry.year * entry.annualInvestment + initialInvestment)
-								)}
-							</td>
-							<td>
-								{formatter.format(
-									entry.year * entry.annualInvestment + initialInvestment
-								)}
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</>
+				))}
+			</tbody>
+		</table>
 	);
 }
