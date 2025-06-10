3bb9886e06606a11d0e41565ffb94f6ab04e48cc
Bronwyn Taljaard <bronwyntaljaard@bronwyns-laptop.home>
2025-06-10 14:42:24 +0200

    cleaned up comments

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -21,7 +21,6 @@ export default function Result({ result, initialInvestment }) {
 							<td>{formatter.format(entry.valueEndOfYear)}</td>
 							<td>{formatter.format(entry.interest)}</td>
 							<td>
-								{/* Need to clean up code, rename functions correctly in App.jsx */}
 								{formatter.format(
 									entry.valueEndOfYear -
 										(entry.year * entry.annualInvestment + initialInvestment)
